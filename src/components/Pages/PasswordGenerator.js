46d3b304782fd8ad3aa78c3dfd1f1c97bccf2f68
SId02 <31335947+SId02@users.noreply.github.com>
2023-09-27 13:42:34 +0530

    add
    changes and add

--- src/components/Pages/PasswordGenerator.js
+++ src/components/Pages/PasswordGenerator.js
@@ -1,6 +1,43 @@
-import React from "react";
+import { useState, useCallback, useEffect, useRef } from 'react'
+
 
 const PasswordGenerator = () => {
+	const [length, setLength] = useState(8)
+	const [numberAllowed, setNumberAllowed] = useState(false);
+	const [symbolAllowed, setSymbolAllowed] = useState(false);
+	const [password, setPassword] = useState("")
+
+//useRef hook
+const passwordRef = useRef(null)
+
+const passwordGenerator = useCallback(() => {
+  let pass = ""
+  let letter = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  if (numberAllowed) letter += "0123456789"
+  if (symbolAllowed) letter += "!@#$%&*"
+
+  for (let i = 1; i <= length; i++) {
+	let char = Math.floor(Math.random() * letter.length + 1)
+	pass += letter.charAt(char)
+	
+  }
+
+  setPassword(pass)
+
+
+}, [length, numberAllowed, symbolAllowed, setPassword])
+
+const copyPasswordToClipboard = useCallback(() => {
+  passwordRef.current?.select();
+  passwordRef.current?.setSelectionRange(0, 999);
+  window.navigator.clipboard.writeText(password)
+}, [password])
+
+useEffect(() => {
+  passwordGenerator()
+}, [length, numberAllowed, symbolAllowed, passwordGenerator])
+
+
 	return (
     <>
       <section>
@@ -19,10 +56,14 @@ const PasswordGenerator = () => {
 									<input
 										className="input"
 										type="text"
-										placeholder="Find a repository"
+											placeholder="Password"
+											value={ password }
+											readOnly
+											ref={passwordRef}
 									/>
-								</p>
-								<button className="button">
+									</p>
+									
+								<button className="button"  onClick={copyPasswordToClipboard}>
 									<span className="icon is-small">
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
@@ -55,21 +96,36 @@ const PasswordGenerator = () => {
 										type="range"
 										id="length"
 										name="length"
-										min="8"
-										max="30"
-										className="mr-2"
+										min={8}
+										max={30}
+										value={length}
+											className="mr-2"
+											onChange={(e) => {setLength(e.target.value)}}
 									/>
-									<label for="length">Length</label>
+									<label for="length">Length: {length}</label>
 								</div>
 								<div>
-									<label className="checkbox numbers">
-										<input type="checkbox" className=" mr-2" />
+									<label className="checkbox numbers" htmlFor="numberInput">
+											<input type="checkbox"
+												  defaultChecked={numberAllowed}
+												 id="numberInput"className=" mr-2"
+											onChange={() => {
+												setNumberAllowed((prev) => !prev);
+											}}
+											
+											/>
 										Numbers
 									</label>
 								</div>
 								<div>
-									<label className="checkbox symbols">
-										<input type="checkbox" className=" mr-2" />
+									<label className="checkbox symbols" htmlFor="characterInput">
+											<input type="checkbox" className=" mr-2"
+											defaultChecked={symbolAllowed}
+											id="characterInput"
+											onChange={() => {
+												setSymbolAllowed((prev) => !prev )
+											}}
+											/>
 										Symbols
 									</label>
 								</div>
@@ -83,9 +139,10 @@ const PasswordGenerator = () => {
         <div className="columns is-mobile">
             <div className="column is-half is-offset-one-quarter">
               <div className="is-centered">
-                <p className="is-text">Length Min 8  to Max 30</p>
-                <p className="is-text">Numbers 0 to 9</p>
-                <p className="is-text">Symbols !@#$%&*? </p>
+			<h3 className="is-text">Length -   Min 8  to Max 30 </h3>
+				<h3 className="is-text">Letters - A to Z a to z </h3>
+                <h3 className="is-text">Numbers - 0 to 9 </h3>
+                <h3 className="is-text">Symbols - !@#$%&* </h3>
               </div>
             </div>
             </div>
